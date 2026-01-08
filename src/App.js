0241c2c2b45d9518f08d47d65f761882c3389dc3
EL-MANSSOURI MOHAMMED <MOHAMMED.EL-MANSSOURI@CMCTTA.MA>
2026-01-08 18:26:34 +0100

    json-server testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,75 @@ import MoviesList from './components/MoviesList';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import MovieDetails from './components/MovieDetails';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const userRef = useRef();
+  const roleRef = useRef();
+  const btnEdit = useRef();
+  const btnDelete = useRef();
+  const [users, setUsers] = useState([]);
+  const [action, setAction] = useState("Add");
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+      .then(res => res.json())             
+      .then(data => {
+        setUsers(data);
+      })
+  },[])
+
+  const usersList = users.map(u => {
+    return (
+      <tr>
+        <td>{u.username}</td>
+        <td>{u.role}</td>
+        <td>
+        <button onClick={() =>{
+          userRef.current.focus();
+          setAction("Edit");
+        } 
+          
+        } ref={btnEdit}>Edit</button>
+          <button onClick={() => handleDelete(u.id)} ref={btnDelete}>Delete</button>
+        </td>
+      </tr>
+    )
+  })
+
+  function handleDelete(id){
+    fetch(`http://localhost:3001/users/${id}`, { 
+      method: "DELETE"
+    })
+      .then(() => {
+        console.log("Delete a user");
+        setUsers(prev => prev.filter(u => u.id !== id));
+      })
+      .catch(err => console.error(err));
+  }
+
+  function handleAdd(e){
+    e.preventDefault();
+    const user = {
+      username: userRef.current.value,
+      role: roleRef.current.value
+    };
+
+    fetch("http://localhost:3001/users", {
+      method: "POST",              
+      headers: {
+        "Content-Type": "application/json"  
+      },
+      body: JSON.stringify(user)    
+    })
+      .then(res => res.json())             
+      .then(data => {
+        console.log("User added :", data);
+        setUsers([...users, user]);
+        userRef.current.value = '';
+        roleRef.current.value = 'Admin';
+      })
+      .catch(err => console.error(err));
+  }
   return (
     <div className="App">
       <Navbar/>
@@ -14,6 +81,23 @@ function App() {
         <Route path='/movies' element={<MoviesList/>}/>
         <Route path='/movies-details/:id' element={<MovieDetails/>}/>
       </Routes>
+      <form onSubmit={handleAdd}>
+        <input type='text' placeholder='username' ref={userRef}/>
+        <select ref={roleRef}>
+          <option value={'Admin'}>Admin</option>
+          <option value={'Super Admin'}>Super Admin</option>
+          <option value={'User'}>User</option>
+        </select>
+        <button>Add user</button>
+      </form>
+      <table>
+        <tr>
+          <th>Username</th>
+          <th>Role</th>
+          <th>Actions</th>
+        </tr>
+        {usersList}
+      </table>
     </div>
   );
 }
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -5,24 +5,29 @@ export default function MovieDetails(){
     const {id} = useParams();
     const API_KEY = '078720dca783d28602bfaaaff5501bdf';
     const [movie, setMovie] = useState({});
-    let genres = [];
+    // const [genres, setGenres] = useState([]);
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
         .then(res => res.json())
         .then(data => {
             setMovie(data);
+            // setGenres(data.genres.map(genre => genre.name))
         })
     },[])
     useEffect(() => {
-        console.log(movie);
+        // console.log(movie);
+        // console.log(movie.genres)
     }, [movie])
     return (
-        <div>
+        <div className="movie">
             <div>
                 <a href={movie.homepage}><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}.`} alt="poster" loading="lazy"/></a>
                 <div>
                     <h2>{movie.title} {new Date(movie.release_date).getFullYear()}</h2>
-                    <p>{movie.tagline}</p>
+                    <div>
+                        {/* {genres.map((g,index) => <span key={index}>{g.name}</span>)} */}
+                    </div>
+                    <cite>{movie.tagline}</cite>
                     <div>
                         <h3>Synopsis</h3>
                         <p>{movie.overview}</p>
